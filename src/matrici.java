76acd654f90123e46811ab39ba42930824ed9c89
Roberto Spotti <roberto.spotti@gmail.com>
2018-10-01 10:00:10 +0200

    Versione iniziale

---
+++ src/matrici.java
@@ -0,0 +1,23 @@
+
+public class matrici {
+
+	
+	public static void main(String[] args) {
+		int [][] mat;
+		int [] riga;
+		int [] colonna;
+		// dimeniona e crea la matrice mat
+		// riempi la matrice
+		// visualizza il menu 
+		// leggi la scelta
+		// Scelta == 1 -- Visualizza elemento massimo
+		// Scelta == 2 -- Visualizza elemento minimo
+		// Scelta == 3 -- Calcola totale per riga
+		// Scelta == 4 -- Calcola totale per colonna
+		// Scelta == 5 -- Visualizza vettori riga e vettori colonna (solo se 3 e/o 4 sono stati esegiti)
+		// Scelta == 6 -- Matrice random con numeri non doppi per riga
+		// Scelta == 7 -- Matrice random con numeri non doppi per colonna
+		// Scelta == 8 -- Matrice random con numeri non doppi 
+	}
+
+}
